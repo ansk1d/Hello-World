16256964dc1e7069a89cb91003d00f9005185d6d
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-06-05 13:55:55 +0300

    Hello PY!

---
+++ Hello_World.py
@@ -0,0 +1,30 @@
+print("Hi Ningas!")
+
+myName = "Anas"
+print("Hello ", myName, "!")
+
+print("Hello "+ myName +"!")
+
+num = 7
+print("Hello",num)
+print("Hello "+ str(num))
+
+
+if myName == "Anas":
+    print("Yaa!")
+else:
+    print("Noo")
+
+favfrut1 ="Apple"
+favfrut2 ="Orange"
+
+print("I love to eat {} and {}.".format(favfrut1, favfrut2))
+# print(f"I love to eat {favfrut1} and {favfrut2}.")
+
+
+
+
+
+
+
+
